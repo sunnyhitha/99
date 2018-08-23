d8bc3609f3bd3e7545ab1faf115a0f9e56df9a18
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-23 12:32:09 +0530

    Create 99.java

---
+++ 99.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,c,d;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		d=(a*b)%c;
+		System.out.print(d);
+		// your code goes here
+	}
+}
